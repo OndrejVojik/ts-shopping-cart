60dca997b9ccab2d0d67656d9ad1a9c3256f821b
OndrejVojik <vojik.cz@gmail.com>
2024-10-08 15:43:56 +0200

    17

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Routes, Route } from "react-router-dom"
+// import { Routes, Route } from "react-router-dom"
 import { Container } from "react-bootstrap"
-import { Home } from "./pages/Home"
-import { Store } from "./pages/Store"
-import { About } from "./pages/About"
+// import { Home } from "./pages/Home"
+// import { Store } from "./pages/Store"
+// import { About } from "./pages/About"
 import { Navbar } from "./components/Navbar"
 import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 
@@ -11,11 +11,12 @@ function App() {
     <ShoppingCartProvider>
       <Navbar />
       <Container className="mb-4">
-        <Routes>
+        {/* <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/store" element={<Store/>} />
           <Route path="/about" element={<About/>} />
-        </Routes>
+        </Routes> */}
+        <h1>Fuck you!!!</h1>
       </Container>
     </ShoppingCartProvider>
     )
