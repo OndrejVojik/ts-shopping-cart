287988651dcdf697edf17eb0361c6f9bbed83659
OndrejVojik <vojik.cz@gmail.com>
2024-10-08 13:08:45 +0200

    update setting in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode (development or production)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  // Use the loaded environment variable to set the base path
+  const basePath = env.VITE_BASE_PATH || '/';
+  console.log('basePath', basePath);
+
+  return {
+    plugins: [react()],
+	base: basePath,
+  };
+});
