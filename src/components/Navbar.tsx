7b80e4d2b240bc140be52782e292f5f1aa002cb0
OndrejVojik <vojik.cz@gmail.com>
2024-10-08 15:20:20 +0200

    16

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,8 +9,8 @@ export const Navbar = () => {
         <Container>
             <Nav className="me-auto">
                 <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
-                <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
-                <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
+                <Nav.Link to="store" as={NavLink}>Store</Nav.Link>
+                <Nav.Link to="about" as={NavLink}>About</Nav.Link>
             </Nav>
             {cartQuantity > 0 && (
             <Button onClick={openCart} style={{width: "3rem", height: "3rem", position: "relative"}} variant="outline-primary" className="rounded-circle">
